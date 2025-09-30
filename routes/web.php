3edcb7b063c6e8f0868947444546b76bf2725c89
Jrabara101 <jrabara101@gmail.com>
2025-09-30 10:48:17 +0800

    Added Routes for login and register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use App\Models\Job;
 
 Route::get('/', function () {
@@ -82,3 +85,14 @@ Route::delete('/jobs/{id}', function ($id) {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+
+
+Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
