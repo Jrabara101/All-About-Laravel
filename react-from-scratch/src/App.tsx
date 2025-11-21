1930c06819234a38d5713597b6d2ecb51c4afe73
Johnrick Rabara <158558094+Jrabara101@users.noreply.github.com>
2025-11-21 10:15:52 +0800

    fixing queries

--- react-from-scratch/src/App.tsx
+++ react-from-scratch/src/App.tsx
@@ -6,22 +6,51 @@ import { Shortlist } from "./components/Shortlist";
 import { SongForm } from "./components/SongForm";
 import { SongList } from "./components/SongList";
 import { songs as songsData } from "./data/songs";
-import { useState } from "react";
+
+
 import { Song } from "./types";
+import { ErrorBoundary } from "react-error-boundary";
+import { getSongs } from "./queries";
+import { LoaderCircle } from "lucide-react";
+import { Suspense, use, useState } from "react";
+import { songs as SongData } from "./data/songs";
+
 
 export function App() {
   return (
     <PageWrapper>
       <Container>
         <Header />
-        <Main />
+         <ErrorBoundary
+          fallbackRender={({ error }) => (
+            <div className="mt-12 bg-red-100 p-6 shadow ring ring-black/5">
+              <p className="text-red-500">
+                {error.message}: {error.details}
+              </p>
+            </div>
+          )}
+        >
+          <Suspense
+            fallback={
+              <div className="mt-12 bg-white p-6 shadow ring ring-black/5">
+                <LoaderCircle className="animate-spin stroke-slate-300" />
+              </div>
+            }
+          >
+            <Main />
+          </Suspense>
+        </ErrorBoundary>
       </Container>
     </PageWrapper>
   );
 }
 
+const songpromise = getSongs();
+
+
 function Main() {
-  const [songs, setSongs] = useState<Song[]>(songsData);
+  const apiSongs = use(songpromise);
+  const [songs, setSongs] = useState<Song[]>(apiSongs);
   const [searchQuery, setSearchQuery] = useState("");
   const currentUserId = 1; // You can set this from context/auth
 
